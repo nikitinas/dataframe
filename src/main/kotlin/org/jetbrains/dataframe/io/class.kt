8cbb209752918625b06196c2560a9f981b8c7823
devcrocod <devcrocod@gmail.com>
2021-04-08 16:45:22 +0300

    change exception type to IllegalStateException

--- src/main/kotlin/org/jetbrains/dataframe/io/class.kt
+++ src/main/kotlin/org/jetbrains/dataframe/io/class.kt
@@ -5,7 +5,7 @@ import org.jetbrains.dataframe.stubs.DataFrameToListNamedStub
 import org.jetbrains.dataframe.stubs.DataFrameToListTypedStub
 
 inline fun <reified C> AnyFrame.writeClass(): DataFrameToListTypedStub {
-    require(C::class.java.isInterface) { "Class must represent an interface."}
+    check(C::class.java.isInterface) { "Class must represent an interface."}
     return DataFrameToListTypedStub(this, C::class)
 }
 fun AnyFrame.writeClass(className: String) = DataFrameToListNamedStub(this, className)

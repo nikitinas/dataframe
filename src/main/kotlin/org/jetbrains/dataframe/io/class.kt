82d054ec76d5f8d2a084c5ad6c1d8c3302bea489
devcrocod <devcrocod@gmail.com>
2021-04-08 11:00:49 +0300

    add class type checking for writeClass

--- src/main/kotlin/org/jetbrains/dataframe/io/class.kt
+++ src/main/kotlin/org/jetbrains/dataframe/io/class.kt
@@ -4,5 +4,8 @@ import org.jetbrains.dataframe.AnyFrame
 import org.jetbrains.dataframe.stubs.DataFrameToListNamedStub
 import org.jetbrains.dataframe.stubs.DataFrameToListTypedStub
 
-inline fun <reified C> AnyFrame.writeClass() = DataFrameToListTypedStub(this, C::class)
+inline fun <reified C> AnyFrame.writeClass(): DataFrameToListTypedStub {
+    require(C::class.java.isInterface) { "Class must represent an interface."}
+    return DataFrameToListTypedStub(this, C::class)
+}
 fun AnyFrame.writeClass(className: String) = DataFrameToListNamedStub(this, className)
